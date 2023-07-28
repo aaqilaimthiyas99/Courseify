5f716dbdd4bfdf7c33199e92921883647c4892a4
aaqilaimthiyas99 <73052761+aaqilaimthiyas99@users.noreply.github.com>
2023-07-28 10:51:25 +0530

    frontend_modified

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -16,7 +16,7 @@ export default function Main() {
     </div>
     <div className="mainCenter">
         <div className="mainCenterLeft">
-            <h1 className="title">Unlocking exceptional education is now a breeze!</h1>
+            <h1 className="title"> Unlocking <span className='title-color-change'>exceptional education is now a</span> breeze!</h1>
             <p className="mainText">Stay connected with a single platform for daily updates on free, time-limited courses offered by various websites. Unlock a world of learning opportunities!</p>
             <button className="getFreeTrialBtn">Get Free Trial</button>
         </div>
@@ -24,8 +24,31 @@ export default function Main() {
             <img src="/assets/mainImage.jpg" alt="" className="mainImg" />
         </div>
     </div>
-    <div className="mainFooter"></div>
-    
+    <div className="mainFooter">
+        <div className='container'>
+            <div className="group1">
+                <div className='box1'>
+                    <img src = "assets/Group1.jpg" alt='' className='group1img'/>
+                    <h1 className='num1'> 10k+ </h1>
+                    <p className='def1'> TOTAL COURSES </p>
+                </div>
+            </div>
+            <div className="group2">
+                <div className="box2">
+                    <img src = "assets/Group2.jpg" alt='' className='group2img'/>
+                    <h1 className='num2'> 500+ </h1>
+                    <p className='def2'> EXPERT TUTORS </p>
+                </div>
+            </div>
+            <div className="group3">
+                <div className="box3">
+                    <img src = "assets/Group3.jpg" alt='' className='group3img'/>
+                    <h1 className='num3'> 300k+ </h1>
+                    <p className='def3'> USERS </p>
+                </div> 
+            </div>
+        </div>
+    </div>
     </>
   )
 }
